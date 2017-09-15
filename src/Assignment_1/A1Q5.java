c7ba0f427740acb61589b0d080a89818d9f6c15a
alkam5246 <alkam5246@HRH-IC0027318.wrdsb.ca>
2017-09-15 14:07:08 -0400

    Question 5

---
+++ src/Assignment_1/A1Q5.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment_1;
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+
+/**
+ *
+ * @author alkam5246
+ */
+public class A1Q5 {
+     public static void main(String[] args) {
+
+City Qwerty = new City();
+Robot NPC = new Robot(Qwerty, 0, 0, Direction.SOUTH);
+
+     
+     }
+}
